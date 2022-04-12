a151385b5c05a5c7e017e699d268069b54fac00b
achensetyawati <setyawati@moonlay.com>
2022-04-12 15:26:00 +0700

    laporan packing list

--- src/routes/index.js
+++ src/routes/index.js
@@ -33,7 +33,8 @@ export default [].concat(
   publicRoutes,
   gudangPusatRoutes,
   tokoRoutes,
-  finishingRoutes
+  finishingRoutes,
+  laporanRoutes
 );
 
 // export default [].concat(nmasterRoutes, publicRoutes, gudangPusatRoutes, laporanRoutes, reportRoutes, designerRoutes, merchandiserRoutes, finishingRoutes, generalInventoryRoutes, authRoutes, purchaseRoutes, npurchaseRoutes, nmasterplan, expeditionRoutes, accountingRoutes, customersRoutes, generalSettingRoutes, bateeqshopReportRoutes, promotionRoutes, nmerchandiserRoutes, nsalesRoutes, gpurchasingRoutes, garmentproductionRoutes);
