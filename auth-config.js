f05b657e303ec26d9e45b89707c0da36adaf036f
achensetyawati <setyawati@moonlay.com>
2022-06-02 15:45:00 +0700

    update main.js and auth-config

--- auth-config.js
+++ auth-config.js
@@ -1,6 +1,6 @@
 export default {
     endpoint: "auth",
-    configureEndpoints: ["auth",  "master", "manufacture", "inventory", "inventory-azure", "merchandiser", "md", "sales", "purchasing", "purchasing-azure", "finance", "nmasterplan", "nsales","nmerchandiser", "purchasingJob","garment-production"],
+    configureEndpoints: ["auth", "master", "inventory", "merchandiser", "sales"],
 
     loginUrl: "authenticate",
     profileUrl: "me",
