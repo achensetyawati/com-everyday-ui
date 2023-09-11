267ac6b8576ee27ed92de458d3534be62b0a6a3a
achensetyawati <achenzetya@gmail.com>
2023-09-11 11:39:36 +0700

    copy report from bateeqshop

---
+++ src/routes/everyday-report.js
@@ -0,0 +1,29 @@
+module.exports=[
+    {
+        route: 'laporan-mutasi-barang',
+        name: 'laporan-mutasi-barang',
+        moduleId: './modules/laporan/movement-inventory-all/index',
+        nav: true,
+        title: 'Laporan Mutasi Barang',
+        auth: true,
+        settings: {
+            group: "everyday shop",
+            subGroup: "Report",
+			permission: { "C.01": 1, "MRD.01": 1 },
+            iconClass: "fa fa-dashboard",
+        }
+    },{
+        route: 'laporan-stok',
+        name: 'laporan-stok',
+        moduleId: './modules/laporan/inventory/index',
+        nav: true,
+        title: 'Laporan Stok',
+        auth: true,
+        settings: {
+            group: "everyday shop",
+            subGroup: "Report",
+			permission: { "C.01": 1, "MRD.01": 1 },
+            iconClass: "fa fa-dashboard",
+        }
+    }
+]
