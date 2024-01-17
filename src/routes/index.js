c870af0484a2de977ecb0bfb6c32623d3e3a0519
achensetyawati <achenzetya@gmail.com>
2024-01-17 09:37:28 +0700

    add input master and merchandiser

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,8 +7,8 @@ var authRoutes = require("./auth");
 var finishingRoutes = require("./finishing");
 var generalInventoryRoutes = require("./general-inventory");
 var everydayRoutes = require("./everyday-report");
+var merchandiserRoutes=require("./merchandiser");
 
-// export default [].concat( nmasterRoutes, publicRoutes, gudangPusatRoutes, tokoRoutes, nmerchandiserRoutes, gpurchasingRoutes, garmentproductionRoutes, laporanRoutes, finishingRoutes);
 export default [].concat(
   nmasterRoutes,
   publicRoutes,
@@ -18,10 +18,5 @@ export default [].concat(
   laporanRoutes,
   authRoutes,
   generalInventoryRoutes,
-  everydayRoutes
+  everydayRoutes,merchandiserRoutes
 );
-
-// export default [].concat(nmasterRoutes, publicRoutes, gudangPusatRoutes, laporanRoutes, reportRoutes, designerRoutes, merchandiserRoutes, finishingRoutes, generalInventoryRoutes, authRoutes, purchaseRoutes, npurchaseRoutes, nmasterplan, expeditionRoutes, accountingRoutes, customersRoutes, generalSettingRoutes, bateeqshopReportRoutes, promotionRoutes, nmerchandiserRoutes, nsalesRoutes, gpurchasingRoutes, garmentproductionRoutes);
-
-// export default [].concat(nmasterRoutes, gudangPusatRoutes, finishingRoutes, publicRoutes );
-// export default [].concat(nmasterRoutes, publicRoutes,gudangPusatRoutes, finishingRoutes, purchaseRoutes, npurchaseRoutes, nmasterplan, expeditionRoutes, accountingRoutes, customersRoutes, generalSettingRoutes, bateeqshopReportRoutes, promotionRoutes, nmerchandiserRoutes, nsalesRoutes, gpurchasingRoutes, garmentproductionRoutes);
